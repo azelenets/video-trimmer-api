23178db06a451df44684e947c1d86814c288b3c6
Andrew Zelenets <andrew.zelenets@gmail.com>
2016-09-20 17:39:03 +0300

    fix specs

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Movie do
-  subject { create(:movie) }
-
   it{ is_expected.to validate_presence_of(:start_time) }
   it{ is_expected.to validate_presence_of(:end_time) }
   it { is_expected.to belong_to(:user) }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  subject { create(:user) }
-
   it { is_expected.to have_many(:movies) }
 end
